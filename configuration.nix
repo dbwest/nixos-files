d500c2d2cb0cf94421a874163435a98e31061a33
David West <david.b.west@gmail.com>
2019-05-04 01:18:40 -0400

    add mtr (my traceroute)

--- configuration.nix
+++ configuration.nix
@@ -191,7 +191,7 @@ in
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash.enableCompletion = true;
-  # programs.mtr.enable = true;
+  programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
   
   # make vim the default EDITOR
